768e4a7f005dca52737c83c0bda7acd55eac2b6b
William Han <hankunpeng@gmail.com>
2025-06-30 19:11:50 +0800

    Add additional messages to demonstrate Python's versatility

--- chapter_02/apostrophe.py
+++ chapter_02/apostrophe.py
@@ -1,2 +1,8 @@
 message = "One of Python's strengths is its diverse community."
+message1 = 'I told my friend, "Python is my favorite language!"'
+message2 = "The language 'Python' is named after Monty Python, not the snake."
+message3 = "One of Python's strengths is its diverse and supportive community."
 print(message)
+print(message1)
+print(message2)
+print(message3)
